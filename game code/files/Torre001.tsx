0d1bb9104d5ad08a5da9b477431bbb81eea7a28c
rodri5430 <rodrigomsgoncalves@hotmail.com>
2025-11-27 20:55:54 +0000

    Adicionar ficheiros do jogo

---
+++ game code/files/Torre001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Torre001" tilewidth="32" tileheight="32" tilecount="8" columns="2">
+ <image source="Estética/Torres 1/Torre001.png" width="70" height="130"/>
+</tileset>
